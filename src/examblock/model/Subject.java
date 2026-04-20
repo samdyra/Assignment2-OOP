4949fce1abab2ff113307ebccec1d95021724769
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-04-21 00:36:05 +1000

    feat: finish Subject class

--- src/examblock/model/Subject.java
+++ src/examblock/model/Subject.java
@@ -1,7 +1,5 @@
 package examblock.model;
 
-import examblock.view.components.Verbose;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,12 +19,14 @@ public class Subject implements StreamManager, ManageableListItem {
         registry.add(this, Subject.class);
     }
 
+    @Override
     public void streamOut(BufferedWriter bw, int nthItem) throws IOException {
-        bw.write(nthItem + ". " + this + System.lineSeparator());
+        bw.write(nthItem + ". " + this);
         bw.write(title + System.lineSeparator());
         bw.write("\"" + description + "\"" + System.lineSeparator());
     }
 
+    @Override
     public void streamIn(BufferedReader br, Registry registry, int nthItem) throws IOException, RuntimeException {
         String heading = Utilities.getLine(br);
         if (heading == null) {
@@ -56,45 +56,86 @@ public class Subject implements StreamManager, ManageableListItem {
         this.description = sanitiseDescription(description.substring(1, description.length() - 1));
     }
 
+    @Override
     public String getFullDetail() {
-
+        return title.toUpperCase() + System.lineSeparator()
+                + "\"" + description + "\"" + System.lineSeparator();
     }
 
+    @Override
     public Object[] toTableRow() {
-
+        return new Object[]{title};
     }
 
+    @Override
     public String getId() {
-
+        return title;
     }
 
     public String sanitiseTitle(String text) {
-
+        // remove trailing periods
+        text = text.replaceAll("\\.+$", "");
+        // trim extra spaces
+        text = text.trim();
+        // Split by one or more spaces
+        String[] words = text.split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                result.append(" ");
+            }
+            String word = words[i];
+            // cap first letter, lower the rest
+            result.append(Character.toUpperCase(word.charAt(0)))
+                    .append(word.substring(1).toLowerCase());
+        }
+        return result.toString();
     }
 
     public String sanitiseDescription(String text) {
-
+        // Handle extra spaces
+        text = text.replaceAll("\\s+", " ");
+        text = text.trim();
+        // make sure start with cap
+        if (!text.isEmpty()) {
+            text = Character.toUpperCase(text.charAt(0)) + text.substring(1);
+        }
+        // Ensure ends with period
+        if (!text.isEmpty() && text.charAt(text.length() - 1) != '.') {
+            text = text + ".";
+        }
+        return text;
     }
 
     public String getTitle() {
-
+        return this.title;
     }
 
     public String getDescription() {
-
+        return this.description;
     }
 
+    @Override
     public String toString() {
-
+        return title.toUpperCase() + System.lineSeparator();
     }
 
+    @Override
     public boolean equals(Object o) {
-
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Subject other = (Subject) o;
+        return other.title.equals(this.title)
+                && other.description.equals(this.description);
     }
 
+    @Override
     public int hashCode() {
-
+        return this.title.hashCode()
+                + 2 * this.description.hashCode();
     }
-
-
 }
