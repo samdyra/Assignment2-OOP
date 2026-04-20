81419fabe97429506d71d6ca6231f691915a731c
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-04-20 20:58:16 +1000

    feat: stream in method in subject

--- src/examblock/model/Subject.java
+++ src/examblock/model/Subject.java
@@ -1,5 +1,7 @@
 package examblock.model;
 
+import examblock.view.components.Verbose;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,12 +23,37 @@ public class Subject implements StreamManager, ManageableListItem {
 
     public void streamOut(BufferedWriter bw, int nthItem) throws IOException {
         bw.write(nthItem + ". " + this + System.lineSeparator());
-        bw.write();
-
+        bw.write(title + System.lineSeparator());
+        bw.write("\"" + description + "\"" + System.lineSeparator());
     }
 
     public void streamIn(BufferedReader br, Registry registry, int nthItem) throws IOException, RuntimeException {
+        String heading = Utilities.getLine(br);
+        if (heading == null) {
+            throw new RuntimeException("EOF reading Subject #" + nthItem);
+        }
 
+        var bits = heading.split("\\. ");
+        int index = Utilities.toInt(bits[0], "Number format exception parsing Subject "
+                + nthItem
+                + " header");
+        if (index != nthItem) {
+            throw new RuntimeException("Subject index out of sync!");
+        }
+
+        String title = Utilities.getLine(br);
+        if (title == null) {
+            throw new RuntimeException("EOF reading Subject #" + nthItem);
+        }
+
+        this.title = sanitiseTitle(title);
+
+        String description = Utilities.getLine(br);
+        if (description == null) {
+            throw new RuntimeException("EOF reading Subject #" + nthItem);
+        }
+
+        this.description = sanitiseDescription(description.substring(1, description.length() - 1));
     }
 
     public String getFullDetail() {
