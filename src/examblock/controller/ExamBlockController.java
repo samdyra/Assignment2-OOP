5280349b7fc7fe5844d76288aa27ee7569a69012
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-05-23 17:01:45 +1000

    fix: handleFinalise method in controller

--- src/examblock/controller/ExamBlockController.java
+++ src/examblock/controller/ExamBlockController.java
@@ -149,12 +149,6 @@ public class ExamBlockController {
     }
 
     private void handleFinalise(ExamBlockModel model) {
-        double oldVersion = model.getVersion();
         SessionHandler.finaliseExamBlock(model);
-
-        // only clear dirty if the ver changed
-        if (model.getVersion() > oldVersion) {
-            model.notifyObservers("finalised");
-        }
     }
 }
